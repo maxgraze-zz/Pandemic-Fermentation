ae8dca2dea007769c0dd218c0a83dd8f4e994e50
maxeneg <maxene.graze@gmail.com>
2021-01-19 09:44:13 -0800

    feat(integrating scrolls): getting bugs by integrating scrolling

--- main.js
+++ main.js
@@ -1,33 +1,83 @@
-import scroller from './scroller.js';
+// using d3 for convenience
+import { drawInitial, drawHistoDots, draw2, draw3 } from './chart.js';
+var main = d3.select('main');
+var scrolly = main.select('#scrolly');
+var figure = scrolly.select('figure');
+var article = scrolly.select('article');
+var step = article.selectAll('.step');
 
-let scroll = scroller.container(d3.select('#graphic'));
-
-scroll();
-
-let lastIndex, activeIndex = 0;
-
-//This is where most of the magic happens. Every time the user scrolls, we receive a new index. First, we find all the irrelevant sections, and reduce their opacity. 
-scroll.on('active', function(index){
-    d3.selectAll('.step')
-        .transition().duration(500)
-        .style('opacity', function(d, i) {return i === index ? 1 : 0.1;});
-  //Next, we selection from a range of activationFunctions (which we create), based on the index of the current section. 
-    activeIndex = index;
-    let sign = (activeIndex - lastIndex) < 0 ? -1 : 1;
-    let scrolledSections = d3.range(lastIndex + sign, activeIndex + sign, sign);
-    scrolledSections.forEach(i => {
-        activationFunctions[i]();
-    });
-    lastIndex = activeIndex;
-});
-  //I placed all the functions in an array. Each function corresponds to a different change in the visualisation. One may change the graph into a scatter plot, and another may initiate a force simulation.
 let activationFunctions = [
-    draw1,
+    drawInitial,
+    drawHistoDots,
     draw2,
     draw3,
-    draw4,
-    draw5,
-    draw6,
-    draw7,
-    draw8
 ];
+// initialize the scrollama
+var scroller = scrollama();
+
+// generic window resize listener event
+function handleResize() {
+  // 1. update height of step elements
+    var stepH = Math.floor(window.innerHeight * 0.75);
+    step.style('height', stepH + 'px');
+
+    var figureHeight = window.innerHeight / 2;
+    var figureMarginTop = (window.innerHeight - figureHeight) / 2;
+
+    figure
+        .style('height', figureHeight + 'px')
+        .style('top', figureMarginTop + 'px');
+
+
+  // 3. tell scrollama to update new element dimensions
+    scroller.resize();
+}
+
+
+// scrollama event handlers
+function handleStepEnter(response) {
+
+  // response = { element, direction, index }
+
+  // add color to current step only
+    step.classed('is-active', function(d, i) {
+        return i === response.index;
+    });
+
+  // update graphic based on step
+    figure.select('#graphic')
+        .transition().duration(500)
+        .style('opacity', function(d, i) {return i === i ? 1 : 0.1;});
+
+    activationFunctions[response.index];
+}
+
+// function setupStickyfill() {
+//     d3.selectAll('.sticky').each(function() {
+//         Stickyfill.add(this);
+//     });
+// }
+
+function init() {
+    drawInitial();
+    // setupStickyfill();
+  // 1. force a resize on load to ensure proper dimensions are sent to scrollama
+    handleResize();
+
+    // 2. setup the scroller passing options
+  // 		this will also initialize trigger observations
+  // 3. bind scrollama event handlers (this can be chained like below)
+    scroller.setup({
+        step: '#scrolly article .step',
+        offset: 0.8,
+        debug: true,
+    })
+        .onStepEnter(handleStepEnter);
+
+
+// setup resize event
+    window.addEventListener('resize', handleResize);
+}
+
+// kick things off
+init();
